33b1da9d8d47ff561b3751ffb3bd5fc3a5793c6e
Sanskriti Dabhade <sanskritidabhade@gmail.com>
2025-02-13 16:16:32 -0500

    Initial commit with the MainActivity.kt and SignUpActivity.kt (sign up page only linked yet)

---
+++ app/src/main/java/com/sanskritidabhade/instagramclone/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.sanskritidabhade.instagramclone
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Adjust window insets properly
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Delay navigation to SignUpActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish() // To prevent MainActivity from being kept in the back stack
+        }, 3000)
+    }
+}
